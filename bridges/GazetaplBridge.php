6572891cf73616a39b5ef1134d7c23cf5f2c79ff
piterszpam <piterzspam@gmail.com>
2020-11-24 02:06:23 +0100

    Update Gazeta.pl, przeniesienie funkcji do pliku

--- bridges/GazetaplBridge.php
+++ bridges/GazetaplBridge.php
@@ -27,6 +27,7 @@ class GazetaplBridge extends BridgeAbstract {
 
 	public function collectData()
 	{
+		include 'myFunctions.php';
 //		Warning: https://wiadomosci.gazeta.pl/wiadomosci/7,114884,26406112,dr-hab-wigura-najwiekszym-problemem-nowej-solidarnosci-jest.html
 //		Twitter frame: https://wiadomosci.gazeta.pl/wiadomosci/7,114884,25947207,trzaskowski-za-kidawe-blonska-kiedys-bylo-tusku-musisz.html
 //		error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
@@ -75,94 +76,61 @@ class GazetaplBridge extends BridgeAbstract {
 			return;
 		}
 		
-		$article_wrapper = $article_html->find('SECTION#article_wrapper', 0);
-		foreach($article_wrapper->find('div.art_embed') as $art_embed)
+		$article = $article_html->find('SECTION#article_wrapper', 0);
+
+		$title = trim($article->find('H1#article_title', 0)->plaintext);
+		$timestamp = trim($article->find('TIME', 0)->getAttribute('datetime'));
+		$author = trim($article->find('A[rel="author"]', 0)->plaintext);
+		$tags = returnTagsArray($article, 'LI.tags_item');
+
+		deleteAllDescendantsIfExist($article, 'comment');
+		deleteAllDescendantsIfExist($article, 'SCRIPT');
+		deleteAllDescendantsIfExist($article, 'DIV[id^="banC"]');
+
+		deleteDescendantIfExists($article, 'DIV#sitePath');
+		deleteDescendantIfExists($article, 'DIV.left_aside');
+		deleteDescendantIfExists($article, 'DIV.ban000_wrapper');
+		deleteDescendantIfExists($article, 'DIV.ban001_wrapper');
+		deleteDescendantIfExists($article, 'DIV.right_aside');
+		deleteDescendantIfExists($article, 'DIV.top_section_bg');
+		deleteDescendantIfExists($article, 'DIV.bottom_section_bg');
+		deleteDescendantIfExists($article, 'DIV#adUnit-007-CONTENTBOARD');
+		deleteDescendantIfExists($article, 'DIV.related_image_number_of_photo');
+		deleteDescendantIfExists($article, 'DIV.related_image_open');
+		deleteDescendantIfExists($article, 'SECTION.tags');
+		clearParagraphsFromTaglinks($article, 'P.art_paragraph', array('/\?tag=/'));
+
+
+		foreach($article->find('div.art_embed') as $art_embed)
 		{
-			$this->deleteAncestorIfDescendantExists($art_embed, 'SCRIPT[src*="video.onnetwork.tv"]');
+			deleteAncestorIfDescendantExists($art_embed, 'SCRIPT[src*="video.onnetwork.tv"]');
 			
 			if (FALSE === is_null($art_embed->find('A[href*="twitter.com/user/status/"]', 0)))
 			{
 				$twitter_url = $art_embed->find('a', 0)->getAttribute('href');
-				$twitter_element = $this->getTwitterElement($twitter_url);
-				$art_embed->outertext = $twitter_element->outertext;
+				$twitter_proxy_url = redirectUrl($twitter_url);
+				$art_embed->outertext = 
+					'<strong><br>'
+					.'<a href='.$twitter_url.'>'
+					."Ramka - ".$twitter_url.'<br>'
+					.'</a>'
+					.'<a href='.$twitter_proxy_url.'>'
+					."Ramka - ".$twitter_proxy_url.'<br>'
+					.'</a>'
+					.'<br></strong>';
 			}
 		}
-		foreach($article_wrapper->find('P.art_paragraph A[href*="?tag="]') as $paragraph)
-		{
-			$paragraph->parent->innertext = $paragraph->parent->plaintext;
-		}
-
-		$tags = array();
-		foreach($article_wrapper->find('LI.tags_item') as $tags_item)
-		{
-			$tags[] = trim($tags_item->plaintext);
-		}
-
-		$this->deleteAllDescendantsIfExist($article_wrapper, 'comment');
-		$this->deleteAllDescendantsIfExist($article_wrapper, 'SCRIPT');
-		$this->deleteAllDescendantsIfExist($article_wrapper, 'DIV[id^="banC"]');
-
-		$this->deleteDescendantIfExists($article_wrapper, 'DIV#sitePath');
-		$this->deleteDescendantIfExists($article_wrapper, 'DIV.left_aside');
-		$this->deleteDescendantIfExists($article_wrapper, 'DIV.ban000_wrapper');
-		$this->deleteDescendantIfExists($article_wrapper, 'DIV.ban001_wrapper');
-		$this->deleteDescendantIfExists($article_wrapper, 'DIV.right_aside');
-		$this->deleteDescendantIfExists($article_wrapper, 'DIV.top_section_bg');
-		$this->deleteDescendantIfExists($article_wrapper, 'DIV.bottom_section_bg');
-		$this->deleteDescendantIfExists($article_wrapper, 'DIV#adUnit-007-CONTENTBOARD');
-		$this->deleteDescendantIfExists($article_wrapper, 'DIV.related_image_number_of_photo');
-		$this->deleteDescendantIfExists($article_wrapper, 'DIV.related_image_open');
-		$this->deleteDescendantIfExists($article_wrapper, 'SECTION.tags');
 		
 
 		$this->items[] = array(
 			'uri' => $url_article,
-			'title' => trim($article_wrapper->find('H1#article_title', 0)->plaintext),
-			'timestamp' => $article_wrapper->find('TIME', 0)->getAttribute('datetime'),
-			'author' => $article_wrapper->find('A[rel="author"]', 0)->plaintext,
-			'content' => $article_wrapper,
+			'title' => $title,
+			'timestamp' => $timestamp,
+			'author' => $author,
+			'content' => $article,
 			'categories' => $tags
 		);
 	}
 
-	private function getTwitterElement($twitter_url)
-	{
-		$twitter_proxy = 'nitter.net';
-		$twitter_url = str_replace('twitter.com', $twitter_proxy, $twitter_url);
-		$html_twitter = getSimpleHTMLDOM($twitter_url);
-		$main_tweet = $html_twitter->find('DIV#m.main-tweet', 0);
-		foreach($main_tweet->find('a') as $element)
-		{
-			$element_url = $element->getAttribute('href');
-			if(strpos($element_url, '/') === 0)
-			{
-				$element_url = "https://".$twitter_proxy.$element_url;
-				$element->setAttribute('href', $element_url);
-			}
-		}
-		$date_text = $main_tweet->find('p.tweet-published', 0)->plaintext;
-		$main_tweet->find('p.tweet-published', 0)->outertext = '<a href="'.$twitter_url.'" title="'.$date_text.'">'.$date_text.'</a>';
-		$main_tweet->find('SPAN.tweet-date', 0)->outertext = '';
-		$main_tweet->find('DIV.tweet-stats', 0)->outertext = '';
-		$main_tweet->find('A.fullname', 0)->outertext = '';
-		return $main_tweet;
-	}
 
-	private function deleteDescendantIfExists($ancestor, $descendant_string)
-	{
-		if (FALSE === is_null($descendant = $ancestor->find($descendant_string, 0)))
-			$descendant->outertext = '';
-	}
-	
-	private function deleteAllDescendantsIfExist($ancestor, $descendant_string)
-	{
-		foreach($ancestor->find($descendant_string) as $descendant)
-			$descendant->outertext = '';
-	}
-
-	private function deleteAncestorIfDescendantExists($ancestor, $descendant_string)
-	{
-		if (FALSE === is_null($descendant = $ancestor->find($descendant_string, 0)))
-			$ancestor->outertext = '';
-	}
 }
---
+++ bridges/myFunctions.php
@@ -0,0 +1,170 @@
+<?php
+	function formatAampLinks($article)
+	{
+		foreach($article->find('amp-iframe') as $amp_iframe)
+		{
+			if(FALSE === is_null($src = ($amp_iframe->getAttribute('src'))))
+			{
+				$src = $amp_iframe->src;
+				$amp_iframe->outertext = 
+				'<strong><br>'
+				.'<a href='.$src.'>'
+				."Ramka - ".$src.'<br>'
+				.'</a>'
+				.'<br></strong>';
+			}
+		}
+		foreach($article->find('amp-twitter') as $amp_twitter)
+		{
+			if(FALSE === is_null($data_tweetid = ($amp_twitter->getAttribute('data-tweetid'))))
+			{
+				$twitter_url = 'https://twitter.com/anyuser/status/'.$data_tweetid;
+				$twitter_proxy_url = redirectUrl($twitter_url);
+				$amp_twitter->outertext = 
+					'<strong><br>'
+					.'<a href='.$twitter_url.'>'
+					."Ramka - ".$twitter_url.'<br>'
+					.'</a>'
+					.'<a href='.$twitter_proxy_url.'>'
+					."Ramka - ".$twitter_proxy_url.'<br>'
+					.'</a>'
+					.'<br></strong>';
+			}
+		}
+		foreach($article->find('amp-youtube') as $amp_youtube)
+		{
+			if(FALSE === is_null($data_videoid = ($amp_youtube->getAttribute('data-videoid'))))
+			{
+				$youtube_url = 'https://www.youtube.com/watch?v='.$data_videoid;
+				$youtue_proxy_url = redirectUrl($youtube_url);
+				$amp_youtube->outertext = 
+					'<strong><br>'
+					.'<a href='.$youtube_url.'>'
+					."Ramka - ".$youtube_url.'<br>'
+					.'</a>'
+					.'<a href='.$youtue_proxy_url.'>'
+					."Ramka - ".$youtue_proxy_url.'<br>'
+					.'</a>'
+					.'<br></strong>';
+			}
+		}
+	}
+
+	function clearParagraphsFromTaglinks($article, $paragrapghSearchString, $regexArray)
+	{
+		foreach($article->find($paragrapghSearchString) as $paragraph)
+			foreach($paragraph->find('A') as $a_element)
+				foreach($regexArray as $regex)
+					if(1 === preg_match($regex, $a_element->href))
+						$a_element->outertext = $a_element->plaintext;
+	}
+	
+	function deleteAncestorIfContainsText($ancestor, $descendant_string)
+	{
+		if (FALSE === is_null($ancestor))
+			if (FALSE !== strpos($ancestor->plaintext, $descendant_string))
+				$ancestor->outertext = '';
+	}
+
+	function parse_article_data($article_data)
+	{
+		if (TRUE === is_object($article_data))
+		{
+			$article_data = (array)$article_data;
+			foreach ($article_data as $key => $value)
+				$article_data[$key] = parse_article_data($value);
+			return $article_data;
+		}
+		else if (TRUE === is_array($article_data))
+		{
+			foreach ($article_data as $key => $value)
+				$article_data[$key] = parse_article_data($value);
+			return $article_data;
+		}
+		else
+			return $article_data;
+	}
+
+	function deleteAncestorIfChildMatches($element, $hierarchy)
+	{
+		$last = count($hierarchy)-1;
+		$counter = 0;
+		foreach($element->find($hierarchy[$last]) as $found)
+		{			
+			$counter++;
+			$iterator = $last-1;
+			while ($iterator >= 0 && $found->parent->tag === $hierarchy[$iterator])
+			{
+				$found = $found->parent;
+				$iterator--;
+			}
+			if ($iterator === -1)
+			{
+				$found->outertext = '';
+			}
+		}
+	}
+	
+	function getTwitterElement($twitter_url)
+	{
+		$twitter_proxy = 'nitter.net';
+		$twitter_url = str_replace('twitter.com', $twitter_proxy, $twitter_url);
+		$html_twitter = getSimpleHTMLDOM($twitter_url);
+		$main_tweet = $html_twitter->find('DIV#m.main-tweet', 0);
+		foreach($main_tweet->find('a') as $element)
+		{
+			$element_url = $element->getAttribute('href');
+			if(strpos($element_url, '/') === 0)
+			{
+				$element_url = "https://".$twitter_proxy.$element_url;
+				$element->setAttribute('href', $element_url);
+			}
+		}
+		$date_text = $main_tweet->find('p.tweet-published', 0)->plaintext;
+		$main_tweet->find('p.tweet-published', 0)->outertext = '<a href="'.$twitter_url.'" title="'.$date_text.'">'.$date_text.'</a>';
+		$main_tweet->find('SPAN.tweet-date', 0)->outertext = '';
+		$main_tweet->find('DIV.tweet-stats', 0)->outertext = '';
+		$main_tweet->find('A.fullname', 0)->outertext = '';
+		return $main_tweet;
+	}
+
+	function deleteDescendantIfExists($ancestor, $descendant_string)
+	{
+		if (FALSE === is_null($descendant = $ancestor->find($descendant_string, 0)))
+			$descendant->outertext = '';
+	}
+	
+	function deleteAllDescendantsIfExist($ancestor, $descendant_string)
+	{
+		foreach($ancestor->find($descendant_string) as $descendant)
+			$descendant->outertext = '';
+	}
+
+	function deleteAncestorIfDescendantExists($ancestor, $descendant_string)
+	{
+		if (FALSE === is_null($descendant = $ancestor->find($descendant_string, 0)))
+			$ancestor->outertext = '';
+	}
+
+	function redirectUrl($social_url)
+	{
+		$twitter_proxy = 'nitter.net';
+		$instagram_proxy = 'bibliogram.art';
+		$facebook_proxy = 'mbasic.facebook.com';
+		$youtube_proxy = 'invidious.snopyta.org';
+		$social_url = preg_replace('/.*[\.\/]twitter\.com(.*)/', 'https://'.$twitter_proxy.'${1}', $social_url);
+		$social_url = preg_replace('/.*[\.\/]instagram\.com(.*)/', 'https://'.$instagram_proxy.'${1}', $social_url);
+		$social_url = preg_replace('/.*[\.\/]facebook\.com(.*)/', 'https://'.$facebook_proxy.'${1}', $social_url);
+		$social_url = preg_replace('/.*[\.\/]youtube\.com(.*)/', 'https://'.$youtube_proxy.'${1}', $social_url);
+		return $social_url;
+	}
+
+	function returnTagsArray($article, $tag_selector)
+	{
+		$tags = array();
+		foreach($article->find($tag_selector) as $tags_item)
+		{
+			$tags[] = trim($tags_item->plaintext);
+		}
+		return $tags;
+	}
