77a964eda5120ee60fa19350f8e8b2b526102ddc
piterszpam <piterzspam@gmail.com>
2020-12-02 04:47:16 +0100

    Dodanie Expendera dla Z3S

---
+++ bridges/MySiteBridge.php
@@ -0,0 +1,37 @@
+<?php
+class MySiteBridge extends FeedExpander {
+
+	const MAINTAINER = 'No maintainer';
+	const NAME = 'MySiteBridge';
+	const URI = '';
+	const DESCRIPTION = 'No description provided';
+	const PARAMETERS = array();
+	const CACHE_TIMEOUT = 1;
+
+    public function collectData(){
+		include 'myFunctions.php';
+        $this->collectExpandableDatas('https://zaufanatrzeciastrona.pl/feed/');
+    }
+
+	protected function parseItem($newsItem)
+	{
+		$item = parent::parseItem($newsItem);
+		$articlePage = getSimpleHTMLDOM($newsItem->link);
+		$article = $articlePage->find('DIV#main DIV.postcontent', 0);
+		foreach($article->find('IMG') as $img)
+		{
+			if ($img->hasAttribute('src'))
+				if (0 === strpos($img->src, '/'))
+					$img->src = 'https://zaufanatrzeciastrona.pl'.$img->src;
+		}
+		foreach($article->find('DIV.wp-block-embed__wrapper') as $embed)
+		{
+			$embed->outertext = '<div class="wp-block-embed__wrapper">'.$embed->innertext.'</div>';
+		}
+		
+		$item['content'] = $article;
+		return $item;
+	}
+
+}
+// Imaginary empty line!
