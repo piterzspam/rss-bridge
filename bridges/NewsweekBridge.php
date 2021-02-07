e59c6bbc4d4a0176e116caa2f999a4ab562ab8ac
piterzspam <piterzspam@users.noreply.github.com>
2021-02-07 23:33:22 +0100

    Update

--- bridges/NewsweekBridge.php
+++ bridges/NewsweekBridge.php
@@ -31,197 +31,218 @@ class NewsweekBridge extends BridgeAbstract {
 
 	public function collectData()
 	{
-		$url_articles_list = $this->getInput('url');
-		$url_articles_list = preg_replace('/(.*\/autorzy\/[a-z]+-[a-z]+).*/', '$1', $url_articles_list);
+		include 'myFunctions.php';
 		$GLOBALS['number_of_wanted_articles'] = $this->getInput('wanted_number_of_articles');
 		
-		$urls = array();
-		$html_articles_list = getSimpleHTMLDOM($url_articles_list);
-		foreach($html_articles_list->find('DIV.authorArticles DIV.smallItem A') as $A)
+		$urls = $this->getArticlesUrls();
+		
+		foreach($urls as $url)
 		{
-			if (count($urls) < $GLOBALS['number_of_wanted_articles'])
-				$urls[] = $A->getAttribute('href');
+			$this->addArticle($url);
+		}
+	}
+
+	private function addArticle($url)
+	{
+		$returned_array = $this->my_get_html($url);
+		if (200 !== $returned_array['code'])
+		{
+			return;
+		}
+		else
+		{
+			$article_html = $returned_array['html'];
 		}
 
-		$page_number = 0;
-		while (count($urls) < $GLOBALS['number_of_wanted_articles'])
+		if (FALSE === is_null($article_html->find('ARTICLE', 0)))
 		{
-			$current_url = $url_articles_list.'?ajax=1&page='.$page_number;
-			$html_articles_list = getSimpleHTMLDOM($current_url);
+			$article = $article_html->find('ARTICLE', 0);
+		}
+		else
+		{
+			return;
+		}
 
-			if (0 !== ($url_counter = count($found_urls = $html_articles_list->find("DIV.smallItem A"))))
+		//author
+		$author = $article->find('H4.name', 0)->plaintext;
+		//tags
+		$tags = array();
+		foreach($article->find('DIV.tags', 0)->find('A') as $tag_element)
+		{
+			$tags[] = trim($tag_element->plaintext);
+			$tag_element->outertext = '';
+		}
+		//date
+		$date = $article->find('SPAN.articleDates', 0)->innertext;
+		$date = str_replace('Data publikacji: ', '', $date);
+		//title
+		$title = trim($article->find('H1.detailTitle', 0)->innertext);
+		if (FALSE === is_null($article_html->find('H1.detailTitle', 0)->find('SPAN.label', 0)))
+		{
+			$title = '[OPINIA] '.$title;
+		}
+
+		//zamiana ramek na linki
+		foreach($article->find('DIV.embeddedApp DIV[data-run-module]') as $data_run_module)
+		{
+			$data_params = $data_run_module->getAttribute('data-params');
+			$hrml_decoded = html_entity_decode($data_params);
+			$params_decoded=json_decode($hrml_decoded, true);
+			$link = redirectUrl($params_decoded['parameters']['url']);
+			$link_element = str_get_html('<a href="'.$link.'">'.$link.'</a>');
+			$data_run_module->outertext = $link_element->outertext;
+		}
+		foreach($article->find('DIV.embeddedApp') as $embeddedApp)
+		{
+			$previous = $embeddedApp->previousSibling();
+			if ('script' === strtolower($previous->tag))
 			{
-				foreach($found_urls as $article_link)
-				{
-					if (count($urls) < $GLOBALS['number_of_wanted_articles'] && FALSE === in_array($link, $urls))
-						$urls[] = $article_link->getAttribute('href');
-				}
-				$page_number++;
+				$previous->outertext = '';
+				$embeddedApp->outertext = '';
 			}
-			else
+		}
+		foreach($article->find('DIV.articleDetail SPAN.descPhoto') as $span)
+		{
+			while (FALSE === is_null($previous = $span->prev_sibling()))
+			{
+				if($previous->tag === 'span')
+				{
+					$next_text=trim($span->innertext);
+					$previous_text=trim($previous->innertext);
+					$new_text=trim($previous_text.' '.$next_text);
+					$previous->innertext = $new_text;
+					$span->outertext='';
+					$span = $previous;
+				}
+				else
+					break;
+			}
+			$span->innertext = trim($span->innertext);
+		}
+		//paragrafy, czytaj inne artykuly
+		foreach($article->find('P') as $paragraph)
+		{
+			$paragraph->innertext = trim($paragraph->innertext);
+			deleteAncestorIfContainsText($paragraph, 'Czytaj też: ');
+			deleteAncestorIfContainsText($paragraph, 'Czytaj także: ');
+			deleteAncestorIfContainsText($paragraph, 'Zobacz też: ');
+		}
+		//Przenoszenie tresci premium poziom wyzej
+		if (FALSE === is_null($offerView = $article->find('DIV.offerView', 0)) && FALSE === is_null($article->find('DIV.contentPremium', 0)))
+		{
+			$offerView = $article->find('DIV.offerView', 0);
+			foreach($article->find('DIV.contentPremium', 0)->childNodes() as $element)
+			{
+				$offerView->outertext = $offerView->outertext.$element->outertext;
+			}
+			deleteDescendantIfExists($article, 'DIV.contentPremium');
+		}
+
+		deleteAllDescendantsIfExist($article, 'comment');
+		deleteAllDescendantsIfExist($article, 'script');
+		deleteAllDescendantsIfExist($article, 'DIV.articleSocials');
+		deleteAllDescendantsIfExist($article, 'DIV.detailFeed');
+		deleteAllDescendantsIfExist($article, 'DIV.bottomArtticleAds');
+		deleteAllDescendantsIfExist($article, 'DIV.onet-ad');
+		deleteAllDescendantsIfExist($article, 'DIV#fbComments');
+		deleteAllDescendantsIfExist($article, 'UL.breadCrumb');
+		deleteAllDescendantsIfExist($article, 'DIV.tags');
+
+		$lead_style = array(
+			'font-weight: bold;'
+		);
+		addStyle($article, 'P.lead', $lead_style);
+		addStyle($article, 'DIV.artPhoto', getStylePhotoParent());
+		addStyle($article, 'PICTURE', getStylePhotoImg());
+		addStyle($article, 'SPAN.descPhoto', getStylePhotoCaption());
+		
+		$this->items[] = array(
+			'uri' => $url,
+			'title' => $title,
+			'timestamp' => $date,
+			'author' => $author,
+			'content' => $article,
+			'categories' => $tags
+		);
+	}
+
+	
+	private function getArticlesUrls()
+	{
+		$articles_urls = array();
+		$url_articles_list = $this->getInput('url');
+		while (count($articles_urls) < $GLOBALS['number_of_wanted_articles'])
+		{
+			$returned_array = $this->my_get_html($url_articles_list);
+			if (200 !== $returned_array['code'])
 			{
 				break;
 			}
-		}
-
-		foreach($urls as $url_article_link)
-		{
-//			$article_html = getSimpleHTMLDOMCached($url_article_link, 86400 * 14);
-			$article_html = getSimpleHTMLDOMCached($url_article_link, 10);
-			if (is_bool($article_html))
-			{
-				$this->items[] = array(
-					'uri' => $url_article_link,
-					'title' => "getSimpleHTMLDOM($url_article_link) jest boolem",
-					'timestamp' => '',
-					'author' => '',
-					'content' => $article_html,
-					'categories' => ''
-				);
-				continue;
-			}
-			if (FALSE === is_null($article_html->find('ARTICLE', 0)))
-			{
-				$article = $article_html->find('ARTICLE', 0);
-			}
 			else
 			{
-				break;
-			}
-
-			//author
-			$author = $article->find('H4.name', 0)->plaintext;
-			//tags
-			$tags = array();
-			foreach($article->find('DIV.tags', 0)->find('A') as $tag_element)
-			{
-				$tags[] = trim($tag_element->plaintext);
-				$tag_element->outertext = '';
-			}
-			//date
-			$date = $article->find('SPAN.articleDates', 0)->innertext;
-			$date = str_replace('Data publikacji: ', '', $date);
-			//title
-			$title = trim($article->find('H1.detailTitle', 0)->innertext);
-			if (FALSE === is_null($article_html->find('H1.detailTitle', 0)->find('SPAN.label', 0)))
-			{
-				$title = '[OPINIA] '.$title;
-			}
-
-			//zamiana ramek na linki
-			foreach($article->find('DIV.embeddedApp DIV[data-run-module]') as $data_run_module)
-			{
-				$data_params = $data_run_module->getAttribute('data-params');
-				$hrml_decoded = html_entity_decode($data_params);
-				$params_decoded=json_decode($hrml_decoded, true);
-				$link = $this->redirectUrl($params_decoded['parameters']['url']);
-				$link_element = str_get_html('<a href="'.$link.'">'.$link.'</a>');
-				$data_run_module->outertext = $link_element->outertext;
-			}
-			foreach($article->find('DIV.embeddedApp') as $embeddedApp)
-			{
-				$previous = $embeddedApp->previousSibling();
-				if ($previous->tag === 'script')
+				$html_articles_list = $returned_array['html'];
+				if (0 === count($found_hrefs = $html_articles_list->find('DIV.smallItem A.elemRelative[href]')))
 				{
-					$previous->outertext = '';
+					break;
 				}
-			}
-			foreach($article->find('DIV.articleDetail SPAN.descPhoto') as $span)
-			{
-				while (FALSE === is_null($previous = $span->prev_sibling()))
+				else
 				{
-					if($previous->tag === 'span')
+					foreach($found_hrefs as $href_element)
 					{
-						$next_text=trim($span->innertext);
-						$previous_text=trim($previous->innertext);
-						$new_text=trim($previous_text.' '.$next_text);
-						$previous->innertext = $new_text;
-						$span->outertext='';
-						$span = $previous;
+						if(isset($href_element->href) && FALSE === in_array($href_element->href, $articles_urls))
+						{
+							$articles_urls[] = $href_element->href;
+						}
 					}
-					else
-						break;
 				}
-				$span->innertext = trim($span->innertext);
-				$span->setAttribute('style', 'display: block;');
+				$url_articles_list = $this->getNextPageUrl($url_articles_list);
 			}
-			//paragrafy, czytaj inne artykuly
-			foreach($article->find('P') as $paragraph)
-			{
-				$paragraph->innertext = trim($paragraph->innertext);
-				$this->deleteAncestorIfContainsText($paragraph, 'Czytaj też: ');
-				$this->deleteAncestorIfContainsText($paragraph, 'Czytaj także: ');
-				$this->deleteAncestorIfContainsText($paragraph, 'Zobacz też: ');
-			}
-			//Przenoszenie tresci premium poziom wyzej
-			if (FALSE === is_null($offerView = $article->find('DIV.offerView', 0)) && FALSE === is_null($article->find('DIV.contentPremium', 0)))
-			{
-				$offerView = $article->find('DIV.offerView', 0);
-				foreach($article->find('DIV.contentPremium', 0)->childNodes() as $element)
-				{
-					$offerView->outertext = $offerView->outertext.$element->outertext;
-				}
-				$this->deleteDescendantIfExists($article, 'DIV.contentPremium');
-			}
-
-			$this->deleteAllDescendantsIfExist($article, 'comment');
-			$this->deleteAllDescendantsIfExist($article, 'script');
-			$this->deleteAllDescendantsIfExist($article, 'DIV.articleSocials');
-			$this->deleteAllDescendantsIfExist($article, 'DIV.detailFeed');
-			$this->deleteAllDescendantsIfExist($article, 'DIV.bottomArtticleAds');
-			$this->deleteAllDescendantsIfExist($article, 'DIV.onet-ad');
-			$this->deleteAllDescendantsIfExist($article, 'DIV#fbComments');
-			$this->deleteAllDescendantsIfExist($article, 'UL.breadCrumb');
-			$this->deleteAllDescendantsIfExist($article, 'DIV.tags');
-//			$this->deleteAllDescendantsIfExist($article, 'SPAN');
-			
-
-
-			$this->items[] = array(
-				'uri' => $url_article_link,
-				'title' => $title,
-				'timestamp' => $date,
-				'author' => $author,
-				'content' => $article,
-				'categories' => $tags
-			);
-//			echo '<br><br><br>article<br>'.$article;
 		}
+		return array_slice($articles_urls, 0, $GLOBALS['number_of_wanted_articles']);
 	}
 
-	private function deleteDescendantIfExists($ancestor, $descendant_string)
+	private function getNextPageUrl($url_articles_list)
 	{
-		if (FALSE === is_null($descendant = $ancestor->find($descendant_string, 0)))
-			$descendant->outertext = '';
+		//https://www.newsweek.pl/autorzy/dominika-dlugosz?ajax=1&page=1
+		if (FALSE !== strpos($url_articles_list, '&page='))
+		{
+			preg_match('/(.*page=)([0-9]+)/', $url_articles_list, $output_array);
+			$url_without__page_number = $output_array[1];
+			$page_number = $output_array[2];
+			$page_number++;
+			$url_articles_list = $url_without__page_number.$page_number;
+		}
+		else
+		{
+			preg_match('/.*newsweek\.pl\/[\/\w-]+/', $url_articles_list, $output_array);
+			$url_articles_list = $output_array[0].'?ajax=1&page=1';
+		}
+		return $url_articles_list;
 	}
-
-	private function deleteAncestorIfDescendantExists($ancestor, $descendant_string)
+	
+	private function my_get_html($url)
 	{
-		if (FALSE === is_null($descendant = $ancestor->find($descendant_string, 0)))
-			$ancestor->outertext = '';
-	}
+		$context = stream_context_create(array('http' => array('ignore_errors' => true)));
+		$page_content = file_get_contents($url, false, $context);
+		$code = getHttpCode($http_response_header);
+		if (200 !== $code)
+		{
+			$html_error = createErrorContent($http_response_header);
+			$date = new DateTime("now", new DateTimeZone('Europe/Warsaw'));
+			$date_string = date_format($date, 'Y-m-d H:i:s');
+			$this->items[] = array(
+				'uri' => $url,
+				'title' => "Error ".$code.": ".$url,
+				'timestamp' => $date_string,
+				'content' => $html_error
+			);
+		}
+		$page_html = str_get_html($page_content);
 
-	private function deleteAncestorIfContainsText($ancestor, $descendant_string)
-	{
-		if (FALSE === is_null($ancestor))
-			if (FALSE !== strpos($ancestor->plaintext, $descendant_string))
-				$ancestor->outertext = '';
-	}
-
-	private function deleteAllDescendantsIfExist($ancestor, $descendant_string)
-	{
-		foreach($ancestor->find($descendant_string) as $descendant)
-			$descendant->outertext = '';
-	}
-
-	private function redirectUrl($url)
-	{
-		$twitter_proxy = 'nitter.net';
-		$instagram_proxy = 'bibliogram.art';
-		$facebook_proxy = 'mbasic.facebook.com';
-		$url = preg_replace('/.*[\.\/]twitter\.com(.*)/', 'https://'.$twitter_proxy.'${1}', $url);
-		$url = preg_replace('/.*[\.\/]instagram\.com(.*)/', 'https://'.$instagram_proxy.'${1}', $url);
-		$url = preg_replace('/.*[\.\/]facebook\.com(.*)/', 'https://'.$facebook_proxy.'${1}', $url);
-		return $url;
+		$return_array = array(
+			'code' => $code,
+			'html' => $page_html,
+		);
+		return $return_array;
 	}
 }
