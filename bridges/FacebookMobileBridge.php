1a76ca3c372d7346c01e24dcd890905e510810cc
piterzspam <piterzspam@users.noreply.github.com>
2021-04-10 18:51:22 +0200

    Update

--- bridges/FacebookMobileBridge.php
+++ bridges/FacebookMobileBridge.php
@@ -315,7 +315,9 @@ class FacebookMobileBridge extends BridgeAbstract {
 			    		'method'=>"GET",
 			    		'header'=>"Accept-language: en\r\n" .
 			            	"Cookie: foo=bar\r\n" .  // check function.stream-context-create on php.net
-			            	"User-Agent: Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:10\r\n" // i.e. An iPad 
+							"User-Agent: Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)\r\n"
+//						    'header'=>"User-Agent:Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)\r\n"
+//			            	"User-Agent: Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:10\r\n" // i.e. An iPad 
 			 		)
 				)
 			);
