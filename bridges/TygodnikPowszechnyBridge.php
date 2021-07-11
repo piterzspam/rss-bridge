cddf550e4b9084b94167ff129f3d08c0d6047d5a
piterzspam <piterzspam@users.noreply.github.com>
2021-07-11 04:40:58 +0200

    Update

--- bridges/TygodnikPowszechnyBridge.php
+++ bridges/TygodnikPowszechnyBridge.php
@@ -71,6 +71,8 @@ class TygodnikPowszechnyBridge extends FeedExpander {
 		}
 		$tags = return_tags_array($article_html, 'DIV#breadcrumb SPAN[typeof="v:Breadcrumb"]');
 		$tags = array_diff($tags, array('Strona główna', $item['title']));
+		$date_published = get_text_from_attribute($article_html, 'META[property="article:published_time"][content]', 'content', "");
+		$date_modified = get_text_from_attribute($article_html, 'META[property="article:modified_time"][content]', 'content', "");
 
 		if (!is_null($premium_element = $article_html->find('DIV.view-id-ltd_warn', 0)))
 		{
@@ -81,27 +83,105 @@ class TygodnikPowszechnyBridge extends FeedExpander {
 			$item['title'] = '[FREE] '.$item['title'];
 		}
 
-		$article = $article_html->find('DIV.view-full-article', 0);
-		$article = replace_part_of_class($article, 'DIV.views-field', 'multiple', 'views-field ', '');
-		$article = foreach_replace_outertext_with_innertext($article, 'DIV.views-field-title');
-		$article = foreach_replace_outertext_with_innertext($article, 'DIV.media-element-container');
-		$article = replace_tag_and_class($article, 'DIV.views-field-field-summary DIV.field-content', 'single', 'STRONG', NULL);
+		$article_html = insert_html($article_html, 'DIV.view-full-article', '<div class="article"><article>', '</article></div>');
+		//print_element($article_html, "article_html");
+		$article = $article_html->find('DIV.article', 0);
+		$article = replace_tag_and_class($article, 'H1.field-content', 'single', 'H1', 'title');
+		$article = replace_tag_and_class($article, 'DIV.views-field.views-field-field-summary', 'single', 'STRONG', 'lead');
+		$article = replace_tag_and_class($article, 'DIV.field-content.views-field-name', 'single', 'DIV', 'authors');
+		$article = replace_tag_and_class($article, 'DIV.authors IMG', 'multiple', 'IMG', 'author photo');
+		$article = replace_tag_and_class($article, 'DIV.authors A', 'multiple', 'A', 'author name link');
+		if (!is_null($author_description = $article->find('DIV.views-field.views-field-field-po-autorze', 0)))
+		{
+			$article = foreach_replace_innertext_with_plaintext($article, "DIV.views-field.views-field-field-po-autorze DIV");
+			if (0 === strlen(trim($author_description->plaintext)))
+			{
+				$author_description->outertext = "";
+				$article = str_get_html($article->save());
+			}
+			else
+			{
+				$article = replace_tag_and_class($article, 'DIV.views-field.views-field-field-po-autorze DIV', 'multiple', 'DIV', 'author description');
+			}
+		}
+		$article = move_element($article, 'H1.title', 'ARTICLE', 'innertext', 'before');
+		$article = insert_html($article, 'H1.title', '', get_date_outertext($date_published, $date_modified));
+		$article = move_element($article, 'STRONG.lead', 'DIV.dates', 'outertext', 'after');
+		$article = move_element($article, 'DIV.author', 'ARTICLE', 'innertext', 'after');
+		$article = insert_html($article, 'DIV.authors IMG.author.photo', '<div class="author photo holder">', '</div>');
+		$article = insert_html($article, 'DIV.authors', '', '', '<HR>');
+		$article = move_element($article, 'DIV.author.description', 'DIV.authors', 'innertext', 'after');
+		$article = move_element($article, 'DIV.authors', 'ARTICLE', 'innertext', 'after');
 
 		$selectors_array = array();
 		$selectors_array[] = 'DIV.views-field-body-1';
 		$selectors_array[] = 'comment';
+		$selectors_array[] = 'SPAN.field-content.initial_char.initial';
+		$selectors_array[] = 'DIV.article-heading';
+		
 		$article = foreach_delete_element_array($article, $selectors_array);
 		$article = combine_two_elements($article, 'DIV.views-field-field-zdjecia', 'DIV.views-field-field-zdjecia-2', 'DIV', 'super_photo');
 
-		$article = foreach_replace_outertext_with_innertext($article, 'qqqqqqqqqqqqqqqqq');
 		$article = format_article_photos($article, 'DIV.super_photo', TRUE, 'src', 'DIV.views-field-field-zdjecia-2 DIV.field-content');
 		$article = format_article_photos($article, 'DIV.file-image', FALSE, 'src', 'DIV.field-item');
+		$article = foreach_replace_outertext_with_innertext($article, 'DIV.field-content');
+		$article = foreach_replace_outertext_with_innertext($article, 'DIV.views-field');
+		$article = foreach_replace_outertext_with_innertext($article, 'DIV.views-row');
+		$article = foreach_replace_outertext_with_innertext($article, 'DIV.view-content');
+		$article = foreach_replace_outertext_with_innertext($article, 'DIV.view-full-article');
+		$article = foreach_replace_outertext_with_innertext($article, 'FIGURE.photoWrapper A');
+
+		//Pętla, bo po usunięciu przesuwają się elementy
+		$strong_counter = 0;
+		$recommended = array("CZYTAJ TAKŻE", "Czytaj także:", "CZYTAJ WIĘCEJ", "Polecamy:");
+		foreach($article->find('ARTICLE P') as $paragraph)
+		{
+			if (!is_null($strong_element = $paragraph->find('STRONG', 0)))
+			{
+				if(check_string_contains_needle_from_array($strong_element->plaintext, $recommended))
+				{
+					$strong_counter++;
+				}
+			}
+		}
+
+		for ($i = 0; $i < $strong_counter; $i++)
+		{
+			foreach($article->find('ARTICLE P') as $paragraph)
+			{
+				if (!is_null($strong_element = $paragraph->find('STRONG', 0)))
+				{
+					if(check_string_contains_needle_from_array($strong_element->plaintext, $recommended))
+					{
+						$previous_element = $paragraph->prev_sibling();
+						$next_element = $paragraph->next_sibling();
+						if (!is_null($previous_element) && !is_null($next_element) && "hr" === strtolower($previous_element->tag) && "hr" === strtolower($next_element->tag))
+						{
+							$paragraph->outertext = "";
+							$previous_element->outertext = "";
+							$next_element->outertext = "";
+							$article = str_get_html($article->save());
+						}
+						else if (!is_null($previous_element) && "hr" === strtolower($previous_element->tag))
+						{
+							$paragraph->outertext = "";
+							$previous_element->outertext = "";
+							$next_element->outertext = "";
+							$article = str_get_html($article->save());
+						}
+					}
+				}
+			}
+		}
+
+		$article = $article->find('ARTICLE', 0);
+
 		$article = add_style($article, 'FIGURE.photoWrapper', getStylePhotoParent());
 		$article = add_style($article, 'FIGURE.photoWrapper IMG', getStylePhotoImg());
 		$article = add_style($article, 'FIGCAPTION', getStylePhotoCaption());
 		$article = add_style($article, 'BLOCKQUOTE', getStyleQuote());
-		$article = replace_part_of_class($article, 'DIV[class*="views-field-field-"]', 'multiple', 'views-field-field-', '');
-		$article = replace_part_of_class($article, 'DIV[class*="views-field-"]', 'multiple', 'views-field-', '');
+//		$article = replace_part_of_class($article, 'DIV[class*="views-field-field-"]', 'multiple', 'views-field-field-', '');
+//		$article = replace_part_of_class($article, 'DIV[class*="views-field-"]', 'multiple', 'views-field-', '');
 
 		$item['content'] = $article;
 		$item['categories'] = $tags;
